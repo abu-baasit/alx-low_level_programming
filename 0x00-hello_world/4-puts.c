4208df19890d0ecb13f96b69d3d5a0d83fa6d8c0
abu-baasit <saheedegunsola@gmail.com>
2023-06-15 19:59:48 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - puts function to print
+ * Return: once its zero 0(Successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
