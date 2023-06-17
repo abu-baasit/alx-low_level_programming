1d187adbf7acf0fb618183b27bf6259f88e3b14d
abu-baasit <saheedegunsola@gmail.com>
2023-06-17 03:51:53 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistdio.h>
+/**
+ * main- Entry point
+ *
+ * description:Program that print an output
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
