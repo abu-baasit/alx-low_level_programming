f1bde5a3cd42a2ab0f58f00159036239ca1405c3
abu-baasit <saheedegunsola@gmail.com>
2023-06-18 16:16:36 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: A program that prints both lower and upper case
+ * Return: Always 0
+ */
+int main(void)
+{	
+int i = 97;
+int j = 65;
+while (i <= 122)
+{
+putchar(i);
+i++;
+}
+while (j <= 90)
+{
+putchar(j);
+j++;
+}
+putchar('\n');
+}
