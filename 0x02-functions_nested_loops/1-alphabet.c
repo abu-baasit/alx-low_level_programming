9637f135b33264b44a803fbda74fac9088514e5b
abu-baasit <saheedegunsola@gmail.com>
2023-06-20 15:49:45 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-* main - print lower case
-* Return: 0
-*
+* main - Entry point
+* print_alphabet - prints the alphabet in lower case followed by a new line
+* Return: Always 0
 */
 void print_alphabet(void)
 {
 int i = 97;
 while (i <= 122)
-{ 
+{
 _putchar(i);
 i++;
 }
