b13a0a4cfc345c107da0ec78dabdee8f127e4001
abu-baasit <saheedegunsola@gmail.com>
2023-06-20 17:15:28 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+* main - Entry point
+* description: A program that print lower case witn exception
+* Return: Always 0
+*/
+int _isalpha(int c)
+{
+if ((c <= 65 && c <= 90) || (c >= 97 && c <= 122))
+{
+return (1);
+}
+return (0);
+}
