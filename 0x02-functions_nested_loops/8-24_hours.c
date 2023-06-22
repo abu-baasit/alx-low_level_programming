c41ba59b5b729293c2b25b1a742fe843a8e25ceb
abu-baasit <saheedegunsola@gmail.com>
2023-06-21 19:54:22 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* jack_bauer - A program that prints timetable
+* Return: two numbers
+*/
+void jack_bauer(void)
+{
+int i;
+int j;
+for (i = 0; i < 24; i++)
+{
+for (j = 0; j < 60; j++)
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j / 10) + '0');
+_putchar('\n');
+}
+}
+}
