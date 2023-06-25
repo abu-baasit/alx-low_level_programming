cda1d49de2f41c34ab480993a53c8c0e1e1283d0
abu-baasit <saheedegunsola@gmail.com>
2023-06-25 03:48:11 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - A program that prints numbers
+* Return: Always 0
+*/
+
+void print_numbers(void)
+{
+int num = 48;
+while (num <= 57)
+{
+_putchar(num);
+num++;
+}
+_putchar('\n');
+}
