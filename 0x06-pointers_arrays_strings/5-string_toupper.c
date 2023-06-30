2d749fd8a7d27387a43aea41949ff99b4345a23f
abu-baasit <saheedegunsola@gmail.com>
2023-06-29 19:45:29 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* string_toupper - functions that changes lower case to upper cases
+* Return: changing lower caae to upper case
+*/
+char *string_toupper(char *)
+{
+int i;
+i = 0;
+while (n[i] >= 'a' && n[i] <= 'z')
+{
+n[i] = n[i] - 32;
+i++;
+}
+return (n);
+}
