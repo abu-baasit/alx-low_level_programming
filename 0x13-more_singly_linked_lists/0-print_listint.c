db807a2caf4fd52a240b586d57a5e49b45005ef6
abu-baasit <saheedegunsola@gmail.com>
2023-07-31 17:44:42 -0700

    more linked list task 1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 /**
 * print_listint - function that prints all the elements
 * @h: linked list
@@ -6,13 +7,12 @@
 */
 size_t print_listint(const listint_t *h)
 {
- size_t i = 0;
-
- while (h)
- {
- printf("%d\n", h->n);
- i++;
- h = h->next;
- }
- return (i);
+size_t i = 0;
+while (h)
+{
+printf("%d\n", h->n);
+i++;
+h = h->next;
+}
+return (i);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
-#ifndef LLSTS_H
+#ifndef LISTS_H
 #define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -30,7 +32,5 @@ listint_t *reverse_listint(listint_t **head);size_t print_listint_safe(const lis
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
-
 
 #endif
