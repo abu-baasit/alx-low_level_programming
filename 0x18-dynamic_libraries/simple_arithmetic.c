bc8d095694686f1854add0b2a102614e1dc6f668
root <abu-baasit>
2023-11-14 09:16:53 -0800

    0x18 dynamic lib task 100

---
+++ 0x18-dynamic_libraries/simple_arithmetic.c
@@ -0,0 +1,55 @@
+/***
+* add - function that adds two numbers
+* @a: first number
+* @b: second number
+* Return:returns the addition of the two numbers
+**/
+int add(int a,int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - function that subtract two numbers
+* @a: first number
+* @b: second number
+* Return:returns the subtraction of the two numbers
+**/
+int sub(int a,int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - function that multiply two numbers
+* @a: first number
+* @b: second number
+* Return:returns the multiplication of the two numbers
+**/
+int mul(int a,int b)
+{
+	return (a * b);
+}
+
+
+/**
+* div - function that divide two numbers
+* @a: first number
+* @b: second number
+* Return:returns the division of the two numbers
+**/
+int div(int a,int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - function that gives remainder of two numbers
+* @a: first number
+* @b: second number
+* Return:returns the remainder of the two numbers
+**/
+int mod(int a,int b)
+{
+	return (a % b);
+}
